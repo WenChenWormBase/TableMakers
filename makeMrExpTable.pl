83e1a85236188305ae15ba466b3c9821d08e6c0b
Wen Chen <wen@wormbase.org>
2014-03-13 10:21:58 -0700

    add experiment type, genotype, species and SRA ID for RNAseq

--- makeMrExpTable.pl
+++ makeMrExpTable.pl
@@ -4,6 +4,7 @@ use strict;
 use Ace;
 
 my @mrExp;
+my @anaExp;
 my @tmp;
 my @stuff;
 my @remark;
@@ -11,7 +12,9 @@ my %PaperGDS;
 my %PaperPMID;
 my %PaperGPL;
 my %mrExpGSM;
-my ($line, $TotalColumns, $r, $m, $gsm, $gse, $paper, $pmid, $p, $platform, $gpl, $condition, $database, $condA, $aoTerm, $lsTerm);
+my ($line, $TotalColumns, $r, $m, $gsm, $gse, $paper, $pmid, $p, $platform, $gpl, $condition, $database, $spe, $type);
+
+my ($condA, $condB, $aoTerm, $aoTermA, $aoTermB, $lsTerm, $lsTermA, $lsTermB, $strain, $geno, $genoA, $genoB);
 
 print "This script create Microarray_experiment table from current WS release.\n";
 
@@ -23,7 +26,7 @@ print "connecting to database... ";
 my $db = Ace->connect(-path => $acedbpath,  -program => $tace) || die print "Connection failure: ", Ace->error;
 
 open (OUT1, ">MrExpTable.csv") || die "cannot open $!\n";
-print OUT1 "Paper\tPMID\tGSE\tPlatform\tExperiment\tGSM\tTissue\tLife_stage\n";
+print OUT1 "Paper\tPMID\tGSE\tPlatform\tExperiment\tType\tGSM\tTissue\tLife_stage\tGenotype\tSpecies\n";
 
 #---------------Get GEO info -----------------------------
 
@@ -90,7 +93,7 @@ $query="find Microarray_experiment";
 
 @mrExp = $db->find($query);
 foreach $m (@mrExp) {
-
+        $type = "microarray"; 
         if ($m->Reference) {
 	    @tmp = $m->Reference;
 	    $paper = $tmp[0];
@@ -102,18 +105,24 @@ foreach $m (@mrExp) {
 		$pmid = "N.A.";
 	    }
 
+	    if ($m-> Species) {
+		$spe = $m-> Species;
+	    } else {
+		$spe = $m-> Species;
+	    }
+	    
 	    if ($PaperGDS{$paper}) {
 		$gse = $PaperGDS{$paper};
 	    } else {
 		$gse = "N.A.";
 	    }
 
-
 	    if ($PaperGPL{$paper}) {
 		$gpl = $PaperGPL{$paper};	
 	    } else {
 		$gpl = "GPL?";
-	    }	    
+	    }
+	    
 	    if ($m->Microarray) {
 	       $p = $m->Microarray;
 	       if ($p =~ /^GPL/) {
@@ -122,7 +131,9 @@ foreach $m (@mrExp) {
 	       } else {
 		   $platform = $p;
 	       }
-	    }	    
+	    } else {
+		$platform = "N.A.";
+	    }	 	    
 	    #$platform = "$gpl($platform)";
 
 
@@ -153,18 +164,25 @@ foreach $m (@mrExp) {
 	}
 
 	#get condition objects
-	if ($m->Microarray_sample) {
-	    $condition = $m->Microarray_sample;
-	} elsif ($m->Sample_A) {
-	    $condition = $m->Sample_A;
-	} else {
-	    $condition = "N.A.";
-	    $aoTerm = "N.A.";
-	    $lsTerm = "N.A.";
-	}
+	if ($m->Microarray_sample) { #single channel experiment
+	    $condition = $m->Microarray_sample; 
 
-	next unless ($condition ne "N.A."); 
-	if ($condition->Tissue) {
+	    if ($condition -> Genotype) {
+		@tmp = $condition -> Genotype;
+		$geno = join "\|", @tmp;
+		@tmp = ();
+	    } elsif ($condition -> Strain) {
+		$strain = $condition -> Strain;
+		if ($strain-> Genotype) {
+		    $geno = $strain -> Genotype;
+		} else {
+		    $geno = $strain;
+		}
+	    } else {
+		$geno = "N.A.";
+	    }
+
+	    if ($condition->Tissue) {
 		@tmp = $condition->Tissue;
 		
 		foreach (@tmp) {
@@ -177,11 +195,11 @@ foreach $m (@mrExp) {
 		$aoTerm = join ",", @tmp;
 		#print "$paper - $aoTerm\n";
 		@tmp = ();
-	} else {
+	    } else {
 		$aoTerm = "N.A.";
-	}
+	    }
 
-	if ($condition -> Life_stage) {
+	    if ($condition -> Life_stage) {
 		@tmp = $condition -> Life_stage;
 
 		foreach (@tmp) {
@@ -193,16 +211,276 @@ foreach $m (@mrExp) {
 
 		$lsTerm = join ",", @tmp;
 		@tmp = ();
-	} else {
+	    } else {
 		$lsTerm = "N.A.";
+	    }
+
+
+	} elsif ($m->Sample_A) { #dual channel experiment
+	    $condA = $m->Sample_A;
+	    $condB =  $m->Sample_B;
+
+            #-------------- Get genotype -----------------
+	    if ($condA -> Genotype) {
+		@tmp = $condA -> Genotype;
+		$genoA = join "\|", @tmp;
+		@tmp = ();
+	    } elsif ($condA -> Strain) {
+		$strain = $condA -> Strain;
+		if ($strain-> Genotype) {
+		    $genoA = $strain -> Genotype;
+		} else {
+		    $genoA = $strain;
+		}
+	    } else {
+		$genoA = "N.A.";
+	    }
+	    
+	    if ($condB -> Genotype) {
+		@tmp = $condB -> Genotype;
+		$genoB = join "\|", @tmp;
+		@tmp = ();
+	    } elsif ($condB -> Strain) {
+		$strain = $condB -> Strain;
+		if ($strain-> Genotype) {
+		    $genoB = $strain -> Genotype;
+		} else {
+		    $genoB = $strain;
+		}
+	    } else {
+		$genoB = "N.A.";
+	    }	    
+	    $geno = join " vs. ", $genoA,, $genoB;
+	    # ------------------done. -----------------------
+
+
+	    # -------------- Get tissue ----------------
+	    if ($condA->Tissue) {
+		@tmp = $condA->Tissue;
+		
+		foreach (@tmp) {
+		    if ($AOName{$_}) {
+			$aname = $AOName{$_};
+			$_ = "$_($aname)";
+		    }
+		}
+
+		$aoTermA = join ",", @tmp;
+		#print "$paper - $aoTerm\n";
+		@tmp = ();
+	    } else {
+		$aoTermA = "N.A.";
+	    }
+
+	    if ($condB->Tissue) {
+		@tmp = $condB->Tissue;
+		
+		foreach (@tmp) {
+		    if ($AOName{$_}) {
+			$aname = $AOName{$_};
+			$_ = "$_($aname)";
+		    }
+		}
+
+		$aoTermB = join ",", @tmp;
+		#print "$paper - $aoTerm\n";
+		@tmp = ();
+	    } else {
+		$aoTermB = "N.A.";
+	    }
+	    $aoTerm = join " vs. ", $aoTermA, $aoTermB;
+	    #------------- done. -----------------
+	    
+
+	    #----- get life stage info ---------------
+	    if ($condA -> Life_stage) {
+		@tmp = $condA -> Life_stage;
+
+		foreach (@tmp) {
+		    if ($LSName{$_}) {
+			$lsname = $LSName{$_};
+			$_ = "$_($lsname)";
+		    }
+		}
+
+		$lsTermA = join ",", @tmp;
+		@tmp = ();
+	    } else {
+		$lsTermA = "N.A.";
+	    }
+	    
+
+	    if ($condB -> Life_stage) {
+		@tmp = $condB -> Life_stage;
+
+		foreach (@tmp) {
+		    if ($LSName{$_}) {
+			$lsname = $LSName{$_};
+			$_ = "$_($lsname)";
+		    }
+		}
+
+		$lsTermB = join ",", @tmp;
+		@tmp = ();
+	    } else {
+		$lsTermB = "N.A.";
+	    }
+	    $lsTerm = join " vs. ", $lsTermA, $lsTermB;
+	    #------------- done. -----------------
+
+
+	} else {
+	    #$condition = "N.A.";
+	    $aoTerm = "N.A.";
+	    $lsTerm = "N.A.";
+	    $geno = "N.A.";
 	}
 
+
 	#print the results
-	print OUT1 "$paper\t$pmid\t$gse\t$gpl($platform)\t$m\t$gsm\t$aoTerm\t$lsTerm\n";
+	if ($platform) {
+	    #do nothing
+	} else {
+	    print "ERROR! $m contains no platform info!\n";
+	}
+	print OUT1 "$paper\t$pmid\t$gse\t$gpl($platform)\t$m\t$type\t$gsm\t$aoTerm\t$lsTerm\t$geno\t$spe\n";
 
 }
-
 print scalar @mrExp, " microarray experiments found in database.\n";
+
+
+$query="QUERY Find Analysis RNAseq* OR TAR*; Title = *";
+
+@anaExp = $db->find($query);
+foreach $m (@anaExp) {
+
+        if ($m->Reference) {
+	    @tmp = $m->Reference;
+	    $paper = $tmp[0];
+	    @tmp = ();
+	    
+	    if ($PaperPMID{$paper}) {
+		$pmid = $PaperPMID{$paper};
+	    } else {
+		$pmid = "N.A.";
+	    }
+
+	    #if ($PaperGDS{$paper}) {
+		#$gse = $PaperGDS{$paper};
+	    #} else {
+		$gse = "N.A.";
+	    #}
+
+	    #if ($PaperGPL{$paper}) {
+		#$gpl = $PaperGPL{$paper};	
+	    #} else {
+		$gpl = "N.A.";
+	    #}
+	    
+	    $gsm = "N.A.";
+	    if ($m -> Database) {
+		        $database = $m->get('Database', 2);
+			if ($database eq "SRA") {
+			    print "SRA entry: $m\n";
+			    $gsm=$m->get('Database', 3);
+			    $gsm = "SRA ID: $gsm";
+			}
+	    }
+	    
+	    if ($m =~ /RNASeq/) {
+		$platform = "RNASeq";
+		$type = "RNASeq";
+	    } else {
+		$platform = "Tiling Array";
+		$type = "Tiling Array";
+	    }	 	    
+
+	} else { # if there is no reference
+	    $paper = "N.A.";
+	    $pmid = "N.A.";
+	    $gse = "N.A.";
+	    $gpl = "N.A.";
+	    $gsm = "N.A.";
+	}
+
+	#get condition objects
+	if ($m->Sample) { #single channel experiment
+	    $condition = $m->Sample; 
+
+	    if ($condition -> Species) {
+		$spe = $condition -> Species;
+	    } else {
+		$spe = "N.A.";
+	    }
+	    
+	    if ($condition -> Genotype) {
+		@tmp = $condition -> Genotype;
+		$geno = join "\|", @tmp;
+		@tmp = ();
+	    } elsif ($condition -> Strain) {
+		$strain = $condition -> Strain;
+		if ($strain-> Genotype) {
+		    $geno = $strain -> Genotype;
+		} else {
+		    $geno = $strain;
+		}
+	    } else {
+		$geno = "N.A.";
+	    }
+
+	    if ($condition->Tissue) {
+		@tmp = $condition->Tissue;
+		
+		foreach (@tmp) {
+		    if ($AOName{$_}) {
+			$aname = $AOName{$_};
+			$_ = "$_($aname)";
+		    }
+		}
+
+		$aoTerm = join ",", @tmp;
+		#print "$paper - $aoTerm\n";
+		@tmp = ();
+	    } else {
+		$aoTerm = "N.A.";
+	    }
+
+	    if ($condition -> Life_stage) {
+		@tmp = $condition -> Life_stage;
+
+		foreach (@tmp) {
+		    if ($LSName{$_}) {
+			$lsname = $LSName{$_};
+			$_ = "$_($lsname)";
+		    }
+		}
+
+		$lsTerm = join ",", @tmp;
+		@tmp = ();
+	    } else {
+		$lsTerm = "N.A.";
+	    }
+
+	} else {
+	    #$condition = "N.A.";
+	    $aoTerm = "N.A.";
+	    $lsTerm = "N.A.";
+	    $geno = "N.A.";
+	    $spe = "N.A.";
+	}
+
+
+	#print the results
+	if ($platform) {
+	    #do nothing
+	} else {
+	    print "ERROR! $m contains no platform info!\n";
+	}
+	print OUT1 "$paper\t$pmid\t$gse\t$gpl($platform)\t$m\t$type\t$gsm\t$aoTerm\t$lsTerm\t$geno\t$spe\n";
+
+}
+print scalar @anaExp, " RNAseq or Tiling array experiments found in database.\n";
+
 
 close (OUT1);
 $db->close();
