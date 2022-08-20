f850a00a56eac1c3624326db289a10cb745af814
Wen Chen <wen@wormbase.org>
2022-08-19 18:04:21 -0700

    fix a typo for ace file of FPKM

--- dumpAceFiles.sh
+++ dumpAceFiles.sh
@@ -5,7 +5,7 @@ setenv ACEDB /home/citace/WS/acedb/
 /usr/local/bin/tace -tsuser 'wen' <<END_TACE
 QUERY FIND Gene WBG*;
 show -a -t Public_name -f GenePublicName.ace
-show -a -t RNASeq_FPKM -f RNASeq_FPMK.ace
+show -a -t RNASeq_FPKM -f RNASeq_FPKM.ace
 QUERY FIND Life_stage;
 show -a -t Public_name -f LS_name.ace
 quit
